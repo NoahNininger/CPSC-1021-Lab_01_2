f7356ed521ed2b2bd39c79ff0c16708e5e087e5c
NoahNininger <114418869+NoahNininger@users.noreply.github.com>
2022-09-26 11:14:41 -0400

    Update bikeRidePoints.cpp

--- bikeRidePoints.cpp
+++ bikeRidePoints.cpp
@@ -4,35 +4,35 @@ using namespace std;
  
 int main()
 { 
-int userInput;  // variable used for user's input, assuming it will be an integer
+	int userInput;  // variable used for user's input, assuming it will be an integer
 
-cout << "Enter the number of bike rides you took this semester: ";
-cin >> userInput;
+	cout << "Enter the number of bike rides you took this semester: ";
+	cin >> userInput;
 
-while (userInput < 0)   // checks for if a number is negative before giving output (input validation)
-{
-    cout << "Number must be between 0 and 5." << endl
-        << "Please re-enter your number." << endl;
-    cin  >> userInput;
-}
+	while (userInput < 0)   // checks for if a number is negative before giving output (input validation)
+	{
+	    cout << "Number must be between 0 and 5." << endl
+		<< "Please re-enter your number." << endl;
+	    cin  >> userInput;
+	}
 
-if (userInput == 0)   // gives user 0 points for 0 bike rides
-	{ cout << "You earned 0 points this semester" << endl; }
+	if (userInput == 0)   // gives user 0 points for 0 bike rides
+		{ cout << "You earned 0 points this semester" << endl; }
 
-else if (userInput == 1)  // gives user 3 points for 1 bike ride
-	{ cout << "You earned 3 points this semester" << endl; }
+	else if (userInput == 1)  // gives user 3 points for 1 bike ride
+		{ cout << "You earned 3 points this semester" << endl; }
 
-else if (userInput == 2)  // gives user 10 points for 2 bike rides
-	{ cout << "You earned 10 points this semester" << endl; }
+	else if (userInput == 2)  // gives user 10 points for 2 bike rides
+		{ cout << "You earned 10 points this semester" << endl; }
 
-else if (userInput == 3)  // gives user 15 points for 3 bike rides
-	{ cout << "You earned 15 points this semester" << endl; }
+	else if (userInput == 3)  // gives user 15 points for 3 bike rides
+		{ cout << "You earned 15 points this semester" << endl; }
 
-else if (userInput == 4)  // gives user 30 points for 4 bike rides
-    { cout << "You earned 30 points this semester" << endl; }
+	else if (userInput == 4)  // gives user 30 points for 4 bike rides
+	    { cout << "You earned 30 points this semester" << endl; }
 
-else if (userInput >= 5)  // gives user 50 points for 5 or more bike rides
-    { cout << "You earned 50 points this semester" << endl; }
+	else if (userInput >= 5)  // gives user 50 points for 5 or more bike rides
+	    { cout << "You earned 50 points this semester" << endl; }
 
-return 0;
+	return 0;
 }
